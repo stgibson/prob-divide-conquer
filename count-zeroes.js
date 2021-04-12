830198a4cb86deff9c409d2837ad7b11a373a8fc
Sean Gibson <seanthomasgibson@gmail.com>
2021-04-12 15:23:14 -0700

    Downloaded starter code & implemented 1 function. Created count-zeroes.js, & downloaded count-zeroes.test.js, find-floor.js, find-floor.test.js, find-rotated-index.js, find-rotated-index.test.js, find-rotation-count.js, find-rotation-count.test.js, jest.config.js, package.json, sorted-frequency.js, & sorted-frequency.test.js. 	new file:   find-rotated-index.js

---
+++ count-zeroes.js
@@ -0,0 +1,24 @@
+function countZeroes(nums) {
+  // find first 0
+  let start = 0;
+  let end = nums.length - 1;
+  while (start <= end) {
+    const middle = Math.floor((beginning + end) / 2);
+    if (nums[middle] === 0) {
+      // if 0 is first num or comes after 1, found first 0
+      if (middle === 0 || nums[middle - 1] === 1) {
+        return nums.length - middle;
+      }
+      else {
+        end = middle - 1;
+      }
+    }
+    else {
+      start = middle + 1;
+    }
+  }
+  // if found no 0, return 0
+  return 0;
+}
+
+module.exports = countZeroes
