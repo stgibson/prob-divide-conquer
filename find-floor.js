1ab662e3f18d714a571273447a746df557903687
Sean Gibson <seanthomasgibson@gmail.com>
2021-04-12 17:25:44 -0700

    Implemented 1 function. Implemented findFloor(nums, target) in find-floor.js.

--- find-floor.js
+++ find-floor.js
@@ -1,5 +1,37 @@
-function findFloor() {
-  
+function findFloor(nums, target) {
+  // base case for floor of target not in nums
+  if (target < nums[0]) {
+    return -1;
+  }
+
+  let start = 0;
+  let end = nums.length - 1;
+
+  while (start <= end) {
+    const middle = Math.floor((start + end) / 2);
+    console.log(middle);
+    if (nums[middle] === target) {
+      return nums[middle];
+    }
+    // if middle number is at the end and less than target, it is floor
+    if (middle === nums.length - 1 && nums[middle] < target) {
+      return nums[middle];
+    }
+    // if middle is last num less than target, it is floor of target
+    if (
+      middle < nums.length - 1 && nums[middle] < target &&
+      nums[middle + 1] > target
+    ) {
+      return nums[middle];
+    }
+    // otherwise if still less than target, remove 1st half
+    if (nums[middle] < target) {
+      start = middle + 1;
+    }
+    else {
+      end = middle - 1;
+    }
+  }
 }
 
 module.exports = findFloor
